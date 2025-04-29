9b661a28bd2650e411b7c61b441178187d3550ca
Anandaffa <anandaffa.apriadi@gmail.com>
2025-04-29 09:56:33 +0700

    prog

--- src/app/components/gallery.tsx
+++ src/app/components/gallery.tsx
@@ -1,27 +1,65 @@
+import { img } from "motion/react-client";
 import Image from "next/image";
-export default function GalleryTest(){
+export default function GalleryTest({returnPage}){
     let gallery=[]
+    const return_ = () => {
+        returnPage('login')
+    }
 
     for (let a=0 ; a < 9 ; a++) {
         gallery.push(
-            <div className="relative aspect-square h-33 max-w-50" key={a}> 
-            <Image
-            src={`https://picsum.photos/seed/picsum${a*123}/400/500`}
-            alt="Waow!!"
-            fill
-            objectFit="cover"
-            className="z-10"
-            />
-        </div>
+        <>
+    <Image
+        className="aspect-3/2 py-2 rounded-2xl object-cover"
+        key={`a_${a + 1}`}
+        src={`https://picsum.photos/seed/picsum${a + 1 * 123}/400/500`}
+        alt=""
+        width={400}
+        height={500}
+    />
+    <Image
+        className="aspect-square py-2 rounded-2xl object-cover"
+        key={`b_${a + 1}`}
+        src={`https://picsum.photos/seed/picsum${a + 1 * 321}/400/500`}
+        alt=""
+        width={400}
+        height={500}
+    />
+    <Image
+        className="aspect-2/3 py-2 rounded-2xl object-cover"
+        key={`c_${a + 1}`}
+        src={`https://picsum.photos/seed/picsum${a + 1 * 69}/400/500`}
+        alt=""
+        width={400}
+        height={500}
+    />
+    <Image
+        className="aspect-square py-2 rounded-2xl object-cover"
+        key={`d_${a + 1}`}
+        src={`https://picsum.photos/seed/picsum${a + 1 * 521}/400/500`}
+        alt=""
+        width={400}
+        height={500}
+    />
+        </>
         )
     }
 
     return(
-        <div className="relative flex justify-center items-center">
-            <div className="columns-3 gap-4">
-            <h1 className="font-mono text-6xl"> Gallery </h1>
-                {gallery}
-            </div>
+        <>
+        {/* <h1 className="font-mono text-6xl text-white"> Gallery </h1> */}
+        <nav className="flex align-bottom justify-between pt-5 px-5">
+            <span className="text-white font-mono text-xl"> Gallery </span>
+            
+            <button 
+            onClick={return_} 
+            className="text-white text-center hover:bg-white hover:text-black transition-colors duration-300 font-mono flex justify-end items-center gap-2 mb-0">
+                <i className="fas fa-arrow-left text-center"></i> Return 
+            </button>
+        </nav>
+        <div className="columns-2xs gap-5 p-5 min-h-[200vh]">
+            {gallery}
         </div>
+        </>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,20 @@ import GalleryTest from "./components/gallery";
 
 export default function Home() {
   const [page_state, pageState] = useState('login')
+  const bg_url = {
+    'login' :'/img/home.png',
+    'profile_irl' :'/img/profile-irl.jpg',
+    'gallery' :'/img/stars_bg.jpg',
+  }
+  const bg_switch = bg_url[page_state] || '/img/stars_bg.jpg'
   const login_handler = (input) =>{
-    // console.log(user,input)
     if (input == 'profile'){pageState('profile_irl')}
     else if (input == 'login') { pageState('login')}
-    // else if (input == 'gallery') { pageState('gallery')}
   }
   return (
     <div className="relative min-h-screen w-screen flex flex-col-reverse bg-black">
-      <main>
 
+      <main>
         <AnimatePresence mode='wait'>
         {/* background image */}
           <motion.div 
@@ -31,7 +35,7 @@ export default function Home() {
               ease: "easeInOut"
           }}>
           <Image
-          src={page_state == 'login' ? "/img/home.png" : "/img/stars_bg.jpg"}
+          src={bg_switch}
           alt="LoginScreen"
           layout="fill"
           objectFit="cover"
@@ -53,29 +57,29 @@ export default function Home() {
             <div className="flex-1 flex z-10">
               <LoginTemplate onLogin={login_handler}/>
             </div>
-          </motion.div>
-          ) : (
-            <motion.div
-            key="profile"
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -50 }}
-            transition={{ duration: 0.5 }}
-            >
-              <div className="flex-1 flex z-10">
-                <GalleryTest/>
-              </div>
-              {/* <ProfileIrlPage pageReturn={login_handler}/> */}
             </motion.div>
-          )
-        }
+            ) : (
+              <motion.div
+              key="profile"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -50 }}
+              transition={{ duration: 0.5 }}
+              className="relative h-full w-2/3 overflow-y-auto justify-center items-center no-scrollbar">
+                <GalleryTest returnPage={login_handler}/>
+                {/* <ProfileIrlPage pageReturn={login_handler}/> */}
+              </motion.div>
+            )
+          }
       </div>
       </AnimatePresence>
       </main>
+
       <footer className=" text-white py-3 z-10 flex justify-between items-center">
         <h3 className="font-mono text-sm text-start mx-5">Background Art by ME, yes i can draw too</h3>
         <h3 className="font-mono text-sm text-end mx-5">2025 GHST_</h3>
       </footer>
+
     </div>
   );
 }
